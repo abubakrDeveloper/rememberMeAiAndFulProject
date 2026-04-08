c2a4ca362a457e02fed1e88841b3086905ee8cd2
abubakrDeveloper <abubakrrashidov42@gmail.com>
2026-04-08 09:12:10 +0500

    coment

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log("ok");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads')); // Rasmlarni saqlash uchun
+
+mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/smart-attendance', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB muvaffaqiyatli ulandi"))
+.catch((err) => console.error("MongoDB ulanishida xato:", err));
+
+app.get('/', (req, res) => {
+    res.send("Smart Attendance API tizimi ishlamoqda...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(` Server ${PORT}-portda ishga tushdi`)
+});
