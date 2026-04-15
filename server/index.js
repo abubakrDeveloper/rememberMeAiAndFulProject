a269d2f880736c387ab31395be4803bc6e4f75db
abubakrDeveloper <abubakrrashidov42@gmail.com>
2026-04-15 10:49:05 +0500

    qilingan kichik xatolik tuzatildi

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads')); // Rasmlarni saqlash uchun
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/smart-attendance')
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB muvaffaqiyatli ulandi"))
 .catch((err) => console.error("MongoDB ulanishida xato:", err));
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
     res.send("Smart Attendance API tizimi ishlamoqda...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(` Server ${PORT}-portda ishga tushdi`)
 });
